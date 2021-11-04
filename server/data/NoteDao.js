f88612a8c4c98560a448fc2f3b0ae4eac365fc0f
Ali Madooei <alimadooei@gmail.com>
2021-11-04 08:17:29 -0400

    Step 4: CRUD Operations

--- server/data/NoteDao.js
+++ server/data/NoteDao.js
@@ -5,15 +5,49 @@ class NoteDao {
     this.notes = [];
   }
 
-  async create(title, text) {}
+  // Pre: title and text are not undefined, and title is not empty
+  async create({ title, text }) {
+    const note = new Note(title, text);
+    this.notes.push(note);
+    return note;
+  }
 
-  async update(id, { title, text }) {}
+  // Pre: id is a valid note ID
+  async update(id, { title, text }) {
+    const index = this.notes.findIndex((note) => note._id === id);
 
-  async delete(id) {}
+    if (title !== undefined) {
+      this.notes[index].title = title;
+    }
 
-  async read(id) {}
+    if (text !== undefined) {
+      this.notes[index].text = text;
+    }
 
-  async readAll(query = "") {}
+    return this.notes[index];
+  }
+
+  // Pre: id is a valid note ID
+  async delete(id) {
+    const index = this.notes.findIndex((note) => note._id === id);
+    const note = this.notes[index];
+    this.notes.splice(index, 1);
+    return note;
+  }
+
+  // Pre: id is a valid note ID
+  async read(id) {
+    return this.notes.find((note) => note._id === id);
+  }
+
+  async readAll(query = "") {
+    if (query !== "") {
+      return this.notes.filter(
+        (note) => note.title === query || note.text === query
+      );
+    }
+    return this.notes;
+  }
 }
 
 module.exports = NoteDao;
