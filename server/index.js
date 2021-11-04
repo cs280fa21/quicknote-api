649296185c4ea022c9bfb42ca7fd933ba366c361
Ali Madooei <alimadooei@gmail.com>
2021-11-04 08:59:04 -0400

    Step 7: Fake data

--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
+const faker = require("faker");
+const NoteDao = require("./data/NoteDao");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+
+const NUM_SAMPLES = 3;
+const notes = new NoteDao();
+for (let i = 0; i < NUM_SAMPLES; i++) {
+  notes.create({
+    title: faker.lorem.sentence(),
+    text: faker.lorem.paragraph(),
+  });
+}
 
 app.get("/", (req, res) => {
   res.send("QuickNote API!");
