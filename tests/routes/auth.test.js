a5183465e37ce44952ccbccd8462a29e14d84843
Ali Madooei <alimadooei@gmail.com>
2021-11-18 11:54:16 -0500

    Step 13: More tests

--- tests/routes/auth.test.js
+++ tests/routes/auth.test.js
@@ -7,7 +7,6 @@ const users = new UserDao();
 const request = supertest(app);
 
 describe("Test authentication endpoints", () => {
-
   describe("Test /authenticate", () => {
     beforeAll(async () => {
       await mongoose.connect(global.__MONGO_URI__);
@@ -19,22 +18,36 @@ describe("Test authentication endpoints", () => {
     });
 
     test("Return 400 when username is missing", async () => {
-      
+      const response = await request.post("/authenticate").send({
+        password: "testclient",
+      });
+      expect(response.status).toBe(400);
     });
 
     test("Return 400 when password is missing", async () => {
-      
+      const response = await request.post("/authenticate").send({
+        username: "testclient",
+      });
+      expect(response.status).toBe(400);
     });
 
     test("Return 403 when username is incorrect", async () => {
-      
+      const response = await request.post("/authenticate").send({
+        username: "client",
+        password: "testclient",
+      });
+      expect(response.status).toBe(403);
     });
 
     test("Return 403 when password is incorrect", async () => {
-      
+      const response = await request.post("/authenticate").send({
+        username: "testclient",
+        password: "client",
+      });
+      expect(response.status).toBe(403);
     });
 
-    test.only("Return 200 when authentication is sucessfull", async () => {
+    test("Return 200 when authentication is sucessfull", async () => {
       const response = await request.post("/authenticate").send({
         username: "testclient",
         password: "testclient",
@@ -43,7 +56,11 @@ describe("Test authentication endpoints", () => {
     });
 
     test("Return a JWT when authentication is sucessfull", async () => {
-
+      const response = await request.post("/authenticate").send({
+        username: "testclient",
+        password: "testclient",
+      });
+      expect(response.body.token).toBeTruthy(); // exists and non empty!
     });
 
     afterAll(async () => {
