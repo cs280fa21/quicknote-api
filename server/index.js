bb7bfce19008b536e46ad15903f94fffa61630dc
Ali Madooei <alimadooei@gmail.com>
2021-11-30 11:12:55 -0500

    Step 12: CORS

--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ const notes = require("./routes/notes.js");
 const users = require("./routes/users.js");
 const auth = require("./routes/auth.js");
 const { globalErrorHandler } = require("./util/middleware");
+const cors = require("cors");
 
 const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("QuickNote API!");
